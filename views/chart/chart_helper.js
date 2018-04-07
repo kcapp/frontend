7877fbbfacbe75942392f68a564d78cae629ac55
thordy <thordendre@gmail.com>
2018-04-07 17:06:10 +0200

    Make lines in charts straight instead of curved to avoid it looking like score increased when 0 points were scored

--- views/chart/chart_helper.js
+++ views/chart/chart_helper.js
@@ -12,34 +12,14 @@ function getChartConfig(title, type, xAxisLabel, yAxisLabel, lables, datasets) {
         },
         options: {
             responsive: true,
-            title: {
-                display: true,
-                text: title
-            },
-            tooltips: {
-                mode: 'index',
-                intersect: false,
-            },
-            hover: {
-                mode: 'nearest',
-                intersect: true
-            },
+            title: { display: true, text: title },
+            tooltips: { mode: 'index', intersect: false, },
+            hover: { mode: 'nearest', intersect: true },
             scales: {
-                xAxes: [{
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: xAxisLabel
-                    }
-                }],
-                yAxes: [{
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: yAxisLabel
-                    }
-                }]
-            }
+                xAxes: [{ display: true, scaleLabel: { display: true, labelString: xAxisLabel } }],
+                yAxes: [{ display: true, scaleLabel: { display: true, labelString: yAxisLabel } }]
+            },
+            elements: { line: { tension: 0 } }
         }
     }
     return config;
