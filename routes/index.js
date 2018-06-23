c947019ef32cfec8dfde0695d6a27f43b00d7c32
thordy <thordendre@gmail.com>
2018-06-23 11:34:32 +0200

    Initial version of dual monitor support for starting screen

--- routes/index.js
+++ routes/index.js
@@ -50,4 +50,47 @@ router.get('/', function (req, res, next) {
         });
 });
 
+router.get('/dual', function (req, res, next) {
+    axios.get(req.app.locals.kcapp.api + '/player/active')
+        .then(response => {
+            var players = response.data
+            players = _.sortBy(players, (player) => player.name);
+            axios.get(req.app.locals.kcapp.api + '/match/modes')
+                .then(response => {
+                    var matchModes = response.data;
+                    axios.get(req.app.locals.kcapp.api + '/owetype')
+                        .then(response => {
+                            var oweTypes = response.data;
+                            axios.get(req.app.locals.kcapp.api + '/match/types')
+                                .then(response => {
+                                    var matchTypes = response.data;
+                                    axios.get(req.app.locals.kcapp.api + '/venue')
+                                        .then(response => {
+                                            var venues = response.data;
+                                            res.render('index_dual', {
+                                                players: players, match_modes: matchModes,
+                                                match_types: matchTypes, owe_types: oweTypes, venues: venues
+                                            });
+                                        }).catch(error => {
+                                            debug('Error when getting venues: ' + error);
+                                            next(error);
+                                        });
+                                }).catch(error => {
+                                    debug('Error when getting owe types: ' + error);
+                                    next(error);
+                                });
+                        }).catch(error => {
+                            debug('Error when getting owe types: ' + error);
+                            next(error);
+                        });
+                }).catch(error => {
+                    debug('Error when getting match modes: ' + error);
+                    next(error);
+                });
+        }).catch(error => {
+            debug('Error when getting leg players: ' + error);
+            next(error);
+        });
+});
+
 module.exports = router;
