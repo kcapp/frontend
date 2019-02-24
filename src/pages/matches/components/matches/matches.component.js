30420dc677141dab9e916eed74aa3a0e61841482
thordy <thordendre@gmail.com>
2019-02-24 12:30:04 +0100

    Added page-header to matches page, and fixed an issue with pagination

--- src/pages/matches/components/matches/matches.component.js
+++ src/pages/matches/components/matches/matches.component.js
@@ -1,22 +1,16 @@
 module.exports = {
-    onCreate(input) {
-        this.state = {
-            total: input.total_pages,
-            page: input.page
-        }
-    },
     onMount() {
         // TODO Improve this to not use jquery twbsPagination
         document.write('<script type="text/javascript" src="/javascripts/jquery.twbsPagination-1.4.1.min.js"><\/script>');
 
-        var total = this.state.total;
-        var page = this.state.page;
+        var total = this.input.total_pages;
+        var currentPage = parseInt(this.input.page_num);
         $(function () {
             $('.sync-pagination').twbsPagination({
                 totalPages: total,
                 visiblePages: 7,
                 initiateStartPageClick: false,
-                startPage: page,
+                startPage: currentPage,
                 cssStyle: '',
                 prevText: '<span aria-hidden="true">&laquo;</span>',
                 nextText: '<span aria-hidden="true">&raquo;</span>',
