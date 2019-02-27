ca7b7e24852d4664e89e5e4cbcab0905dcef9687
thordy <thordendre@gmail.com>
2019-02-27 20:27:48 +0100

    Some fixes to leg-result and match-result

--- src/pages/leg-result/components/leg-result/leg-result.component.js
+++ src/pages/leg-result/components/leg-result/leg-result.component.js
@@ -1,15 +1,66 @@
 var moment = require("moment");
+var axios = require("axios");
+var alertify = require("../../../../util/alertify");
 
 module.exports = {
     onCreate(input) {
         var leg = input.leg;
 
         leg.started = moment(leg.created_at).format('YYYY-MM-DD HH:mm:ss');
-        leg.finished = leg.end_time === undefined ? '-' : moment(leg.end_time).format('YYYY-MM-DD HH:mm:ss');
-        leg.duration = moment.duration(moment(leg.end_time).diff(leg.visits[0].created_at)).asMinutes().toFixed();
-
-        this.state = {
-            leg: leg
+        leg.finished = leg.end_time === null ? '-' : moment(leg.end_time).format('YYYY-MM-DD HH:mm:ss');
+        leg.duration = "-";
+        if (leg.visits.length > 0) {
+            leg.duration = moment.duration(moment(leg.end_time).diff(leg.visits[0].created_at)).asMinutes().toFixed();
         }
+        this.state = {
+            leg: leg,
+            matchId: leg.match_id
+        }
+    },
+    onMount() {
+        document.addEventListener("keydown", this.onKeyDown.bind(this), false);
+    },
+    onKeyDown(e) {
+        switch (e.key) {
+            case 'Enter':
+                this.continueMatch();
+                e.preventDefault();
+                break;
+            case 'Home':
+                location.href = '/';
+                break;
+            case '.':
+            case 'Backspace':
+            case 'PageUp': // Intentional fall-through
+                this.viewMatchResult();
+                break;
+            default: // NOOP
+        }
+        e.preventDefault();
+    },
+    continueMatch(event) {
+        location.href = '/matches/' + this.state.matchId;
+    },
+    viewMatchResult(event) {
+        location.href = '/matches/' + this.state.matchId + '/result';
+    },
+    undoLegFinish(event) {
+        alertify.confirm('Leg will no longer be finalized',
+            () => {
+                axios.put(window.location.origin + '/legs/' + this.state.leg.id + '/undo', null)
+                    .then(response => {
+                        location.href = '/legs/' + this.state.leg.id;
+                    }).catch(error => {
+                        alert('Unable to undo leg, see log for details (' + error.statusText + ')');
+                    });
+            }, () => { /* NOOP */ });
+    },
+    rematch(event) {
+        axios.post(window.location.origin + '/matches/' + this.state.matchId + '/rematch', null)
+            .then(response => {
+                location.href = '/legs/' + response.data.current_leg_id
+            }).catch(error => {
+                alert('Unable to undo leg, see log for details (' + error.statusText + ')');
+            });
     }
 }
--- src/pages/match-result/components/match-result/match-result.component.js
+++ src/pages/match-result/components/match-result/match-result.component.js
@@ -11,5 +11,31 @@ module.exports = {
         this.state = {
             match: match
         }
+    },
+    onMount() {
+        document.addEventListener("keydown", this.onKeyDown.bind(this), false);
+    },
+    onKeyDown(e) {
+        switch (e.key) {
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                var idx = parseInt(e.key) - 1; // Match array index
+                var leg = this.state.match.legs[idx];
+                if (leg) {
+                    location.href = '/legs/' + leg.id + '/result';
+                }
+                break;
+            case 'Home':
+                location.href = '/';
+                break;
+            default: // NOOP
+        }
     }
 }
