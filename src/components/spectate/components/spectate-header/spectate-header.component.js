545d7f239b2b8b1a8b2e46344e0d8f223021e07c
Thord Setsaas <t.setsaas@sportradar.com>
2025-02-27 10:19:12 +0100

    Persist spectate announcements setting

--- src/components/spectate/components/spectate-header/spectate-header.component.js
+++ src/components/spectate/components/spectate-header/spectate-header.component.js
@@ -1,13 +1,20 @@
+const localStorage = require('../../../../util/localstorage');
+
 module.exports = {
-    onInput(input) {
+    onCreate(input) {
         this.state = {
             round: input.round,
             enableAnnouncement: false
         }
     },
+    onMount() {
+        this.state.enableAnnouncement = localStorage.getBool('spectate-announcements', false);
+    },
     enableVoiceAnnouncement(event) {
-        var enable = !this.state.enableAnnouncement;
+        const enable = !this.state.enableAnnouncement;
         this.emit('enable-announcement', enable);
         this.state.enableAnnouncement = enable;
+
+        localStorage.set('spectate-announcements', enable);
     }
 };
